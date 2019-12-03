dfdc7fb93218740a2fdca52b25906f84c21e7534
Ahmad Hakroosh <ahmadh@yad2.co.il>
2019-12-03 11:16:02 +0200

    Initial commit: Vue & nodejs - TypeScript

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+import router from "./routes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.APPLICATION_PORT || 8081;
+
+app.use(router);
+
+app.listen(PORT, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${PORT}`);
+});
