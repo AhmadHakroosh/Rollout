3ef926992d525c50b7a9ac8d65688625c55ce94c
AhmadHakroosh <a.hakrush@gmail.com>
2019-12-04 22:26:47 +0200

    Added api end-points for full CRUD

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,8 +2,24 @@ import { Request, Response, Router  } from "express";
 
 const router = Router();
 
-router.get("/", (request: Request, response: Response) => {
-    response.send( "Hello world!" );
+// Users retreival end-point
+router.get("/users", (request: Request, response: Response) => {
+
+});
+
+// User creation end-point
+router.post("/users", (request: Request, response: Response) => {
+
+});
+
+// User update end-point
+router.put("/users/:id", (request: Request, response: Response) => {
+
+});
+
+// User delete end-point
+router.delete("/users/:id", (request: Request, response: Response) => {
+
 });
 
 export default router;
