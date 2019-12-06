6fcf9b555a6fbb6d0effffcc960fd23967223ecf
AhmadHakroosh <a.hakrush@gmail.com>
2019-12-06 12:48:00 +0200

    replaced " with ' - convention

--- server/src/interfaces/person.interface.ts
+++ server/src/interfaces/person.interface.ts
@@ -1,4 +1,4 @@
-import { Document } from "mongoose";
+import { Document } from 'mongoose';
 
 export interface IPerson extends Document {
     firstname: string;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,18 +1,18 @@
-import { Request, Response, Router  } from "express";
-import { People } from "../controllers/people.controller";
+import { Router  } from 'express';
+import { People } from '../controllers/people.controller';
 
 const router = Router();
 
 // People retrieval end-point
-router.get("/people", People.retrieve);
+router.get('/people', People.retrieve);
 
 // Person creation end-point
-router.post("/people", People.create);
+router.post('/people', People.create);
 
 // Person update end-point
-router.patch("/people/:id", People.update);
+router.patch('/people/:id', People.update);
 
 // Person deletion end-point
-router.delete("/people/:id", People.delete);
+router.delete('/people/:id', People.delete);
 
 export default router;
